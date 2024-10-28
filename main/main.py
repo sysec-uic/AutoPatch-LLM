183f4dc40e94c348d409c7851623440aa18de4a4
simobevilacqua <126004481+simobevilacqua@users.noreply.github.com>
2024-10-28 18:48:16 +0000

    Add afl fuzzer and change project folder structure

--- main/main.py
+++ main/main.py
@@ -5,10 +5,10 @@ afl_fuzzer_path = "../afl-2.52b/afl-fuzz"
 
 def run_sanitizer(program_path):
     # Compile the file and get the sanitizer result
-    executable_name = program_path[2:-2]
+    executable_name = program_path[:-2]
     # -O1 recommended with ASan to reduce false positives
     warnings = "-Wall -Wextra -Wformat -Wshift-overflow -Wcast-align -Wstrict-overflow -fstack-protector-strong"
-    command = f"gcc {program_path} {warnings} -O1 -fsanitize=address -g -o executables/{executable_name}"
+    command = f"gcc codebase/{program_path} {warnings} -O1 -fsanitize=address -g -o executables/{executable_name}"
     result = subprocess.run(
         [command],
         stderr=subprocess.PIPE,
@@ -18,6 +18,8 @@ def run_sanitizer(program_path):
         shell=True
     )
     log = result.stdout + result.stderr
+
+    print(f"1.{result}")
 
     # Save the sanitizer result in the bug log
     command = f"mkdir -p bugLog; echo \"{log}\" > bugLog/{executable_name}.txt"
@@ -30,14 +32,14 @@ def run_sanitizer(program_path):
         shell=True
     )
 
-    print(result.stderr)
+    print(f"2.{result}")
 
     return log
 
 def run_fuzzer(program_path):
     # Compile the file for the fuzzer
-    executable_name = program_path[2:-2]
-    command = f"{afl_compiler_path} {program_path} -o executables_afl/{executable_name}.afl"
+    executable_name = program_path[:-2]
+    command = f"{afl_compiler_path} codebase/{program_path} -o executables_afl/{executable_name}.afl"
     result = subprocess.run(
         [command],
         stderr=subprocess.PIPE,
@@ -46,6 +48,8 @@ def run_fuzzer(program_path):
         timeout=10,
         shell=True
     )
+
+    print(f"3.{result}")
 
     # Run the fuzzer to get the crashes
     #command = f"sh -c '{afl_fuzzer_path} -i input -o output ./executables_afl/{executable_name}.afl'"
@@ -59,7 +63,10 @@ def run_fuzzer(program_path):
         shell=True
     )
 
+    print(f"4.{result}")
+
 def run_file(executable_path, input, inputFromFile=False):
+    executable_name = f"executables/{executable_path}"
     if inputFromFile:
         # If the program takes input from a file
         result = subprocess.run(
@@ -70,6 +77,8 @@ def run_file(executable_path, input, inputFromFile=False):
             timeout=10,
             shell=True
         )
+
+        print(f"5.{result}")
     else:
         # If the program takes input from stdin
         result = subprocess.run(
@@ -89,9 +98,9 @@ def run_file(executable_path, input, inputFromFile=False):
     return output
 
 def main():
-    res = run_sanitizer("./example1.c")
-    res = run_fuzzer("./example1.c")
-    res = run_file("./executables/example1", "Name")
+    res = run_sanitizer("example1.c")
+    res = run_fuzzer("example1.c")
+    res = run_file("example1", "Name")
     print(res)
 
 if __name__ == "__main__":
