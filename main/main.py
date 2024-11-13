7f45afd9d97105347c2fbf9d903efaef9ded9292
simobevilacqua <126004481+simobevilacqua@users.noreply.github.com>
2024-11-13 22:42:49 +0000

    Add testing part in feedback correcting loop

--- main/main.py
+++ main/main.py
@@ -12,12 +12,15 @@ executables_afl_path = "executables_afl/"
 input_path = "input/"
 patched_codes_path = "patched_codes/"
 
-def run_sanitizer(program_path):
+def run_sanitizer(program_path, isCodebase=True):
     # Compile the file and get the sanitizer result
     executable_name = program_path[:-2]
     # -O1 recommended with ASan to reduce false positives
     warnings = "-Wall -Wextra -Wformat -Wshift-overflow -Wcast-align -Wstrict-overflow -fstack-protector-strong"
-    command = f"gcc {codebase_path}{program_path} {warnings} -O1 -fsanitize=address -g -o {executables_path}{executable_name}"
+    if isCodebase:
+        command = f"gcc {codebase_path}{program_path} {warnings} -O1 -fsanitize=address -g -o {executables_path}{executable_name}"
+    else:
+        command = f"gcc {patched_codes_path}{program_path} {warnings} -O1 -fsanitize=address -g -o {executables_path}{executable_name}"
     try:
         result = subprocess.run(
             [command],
@@ -51,10 +54,13 @@ def run_sanitizer(program_path):
     
     return ret, log
 
-def run_fuzzer(program_path):
+def run_fuzzer(program_path, isCodebase=True):
     # Compile the file for the fuzzer
     executable_name = program_path[:-2]
-    command = f"{afl_compiler_path} {codebase_path}{program_path} -o {executables_afl_path}{executable_name}.afl"
+    if isCodebase:
+        command = f"{afl_compiler_path} {codebase_path}{program_path} -o {executables_afl_path}{executable_name}.afl"
+    else:
+        command = f"{afl_compiler_path} {patched_codes_path}{program_path} -o {executables_afl_path}{executable_name}.afl"
     try:
         result = subprocess.run(
             [command],
@@ -101,35 +107,36 @@ def run_file(executable_path, input, inputFromFile=False):
     executable_name = f"./{executables_path}{executable_path}"
     if inputFromFile:
         # If the program takes input from a file
+        command = f"{executable_name} input"
         try:
             result = subprocess.run(
-                [executable_name, input],
+                [command],
                 stderr=subprocess.PIPE,
                 stdout=subprocess.PIPE,
                 universal_newlines=True,
                 timeout=10,
                 shell=True
             )
+            return result.stderr
         except Exception as e:
             print(f"Error: {e}")
+            return "Error"
     else:
         # If the program takes input from stdin
+        command = f"echo '{input}' | {executable_name}"
         try:
             result = subprocess.run(
-                [executable_name],
-                input=input,
+                [command],
                 stderr=subprocess.PIPE,
                 stdout=subprocess.PIPE,
                 universal_newlines=True,
                 timeout=10,
                 shell=True
             )
+            return result.stderr
         except Exception as e:
             print(f"Error: {e}")
-    
-    output = result.stderr + result.stdout
-
-    return output
+            return "Error"
 
 def ask_llm_for_patch(client, code, sanitizer_output=None, crashes=None):
     if crashes is not None:
@@ -162,7 +169,7 @@ def main():
     os.makedirs(patched_codes_path, exist_ok=True)
 
     # Clear old files
-    command = f"rm -rf {bugLog_path}* {executables_path}* {executables_afl_path}* {patched_codes_path}* output*"
+    command = f"rm -rf {bugLog_path}* {executables_path}* {executables_afl_path}* output* {patched_codes_path}*"
     try:
         result = subprocess.run(
             [command],
@@ -177,32 +184,68 @@ def main():
 
     # Analyze the basecode
     for code in os.listdir(codebase_path):
-        print(f"Analyzing code: {code} ...")
-
+        isCodebase = True
+        
         code_name = code[:-2]
         res, log = run_sanitizer(code)
         with open(f"{codebase_path}{code}", 'r') as file:
             content_code = file.read()
-        if res: # The code is syntactically correct, so an executable file exists for it
-            run_fuzzer(code)
-            crashes = extract_crashes(code_name)
-            reply = ask_llm_for_patch(client, content_code, log, crashes)
-        else: # The code is not syntactically correct, so we just give gpt the original code without any additional information
-            reply = ask_llm_for_patch(client, content_code)
         
-        try:
-            patched_code = reply.split("```c")[1].split("```")[0].strip()
-        except Exception as e:
-            print(f"Error: {e}")
-            patched_code = ""
-        
-        # Save patched code
-        with open(f"{patched_codes_path}{code}", "w") as file:
-            file.write(patched_code)
-        
-        print(f"Patched code for {code} created\n\n")
-        
-        #run_file(patched_code, crashes)
+        while True:
+            print(f"Analyzing code: {code} ...")
+            if res: # The code is syntactically correct, so an executable file exists for it
+                run_fuzzer(code, isCodebase)
+                crashes_inputs = extract_crashes(code_name)
+                reply = ask_llm_for_patch(client, content_code, log, crashes_inputs)
+            else: # The code is not syntactically correct, so we just give gpt the original code without any additional information
+                reply = ask_llm_for_patch(client, content_code)
+            
+            try:
+                patched_code = reply.split("```c")[1].split("```")[0].strip()
+            except Exception as e:
+                print(f"Error: {e}")
+                patched_code = ""
+            
+            # Save patched code
+            with open(f"{patched_codes_path}{code}", "w") as file:
+                file.write(patched_code)
+            content_code = patched_code
+            
+            print(f"Patched code for {code} created")
+            isCodebase = False
+
+            # Test patched code
+            print(f"Testing code: {code} ...")
+            hasCrashed = False
+            if res:
+                res, log = run_sanitizer(code, isCodebase)
+                for crash in crashes_inputs:
+                    output = run_file(code[:-2], crash)
+                    if output != "":
+                        hasCrashed = True
+
+                        # Add crash info to bugLog file
+                        command = f"echo \"{output}\" >> {bugLog_path}{code_name}.txt"
+                        try:
+                            result = subprocess.run(
+                                [command],
+                                stderr=subprocess.PIPE,
+                                stdout=subprocess.PIPE,
+                                universal_newlines=True,
+                                timeout=10,
+                                shell=True
+                            )
+                        except Exception as e:
+                            print(f"Error: {e}")
+            else:
+                print("No crashes detected") # Fix the fact that we have no crashes to use for testing
+            
+            if not hasCrashed:
+                print(f"Patched code for {code} correctly working")
+                break
+            else:
+                print(f"Patched code for {code} not properly working")
+
 
 if __name__ == "__main__":
     main()
