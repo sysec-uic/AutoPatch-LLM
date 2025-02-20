5c6a05767290093f84fdbb632cce60d9833d09dd
Robert D. Hernandez <Rhernandez513@users.noreply.github.com>
2025-02-19 19:30:42 -0600

    Feature #30 Add First Github Action - Super Linter  (#29)
    * add first github action for super linter for markdownlint, yamllint, python black, python isort
    
    * Update egress policy from block (default) to audit. . Update checkout to sparce (only root files) plus src and .github directories
    
    * Add github actor name to workflow run name
    
    * update pyproject.toml
    
    * update pyproject toml path for linter
    
    * stat pyproject.toml inside runner
    
    * update super linter action
    
    * update lint env vars
    
    * try not specifying pyproject toml
    
    * try use quotes for super-linter.env
    
    * add workflow dispatch
    
    * remove super-linter.env file in favor of in-line env injection in the GHA
    
    * disable unused linters, check out only needed files
    
    * move config up a level to share between jobs, remove extre unused linters
    
    * disable JSCPD and enable fixxing some linting issues automatically
    
    * disable more, allow continue on error to see if linting pipeline attempts some fixes
    
    * exclude committing super linter summary file
    
    * exclude super linter summary from config
    
    * remove automatic linting fixes, allow devs to fix their own linting issues
    
    * fix linting issues in dockerfiles
    
    * alphabetical order or env var for main
    
    * disable the use of prettier
    
    * markdown linting fixes
    
    * natual language linting fixes
    
    * llm dispatch isort linting fixes
    
    * ENV lint fixes
    
    * markdown lint fixes
    
    * add root README status badge

--- src/llm-dispatch/invoke-chatgpt-API-hello-world-2.py
+++ src/llm-dispatch/invoke-chatgpt-API-hello-world-2.py
@@ -1,8 +1,6 @@
 from openai import OpenAI
+
 client = OpenAI()
 completion = client.chat.completions.create(
-    model="gpt-4o",
-    messages=[
-        {"role": "user", "content": "write a haiku about ai"}
-    ]
+    model="gpt-4o", messages=[{"role": "user", "content": "write a haiku about ai"}]
 )
