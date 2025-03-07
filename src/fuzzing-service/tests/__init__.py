8aef501a8f1c1d0d3edd184923ad31f0c94724df
Robert D. Hernandez <rherna70@uic.edu>
2025-03-07 15:32:23 +0000

    feature(MessageBrokerClient): extract MessageBrokerClient into standalone class and wheel, update mqtt echo component for devtooling

---
+++ src/fuzzing-service/tests/__init__.py
@@ -0,0 +1,9 @@
+TEMP_ENV_VARS = {
+    'DATABRICKS_HOST': 'https://community.cloud.databricks.com/',
+    'DATABRICKS_USERNAME': 'my_test_account@gmail.com',
+    'DATABRICKS_PASSWORD': 'rHjDG9Ll3cXYi7hPRH0h11gp',
+}
+
+ENV_VARS_TO_SUSPEND = [
+    'DATABRICKS_TOKEN',
+]
