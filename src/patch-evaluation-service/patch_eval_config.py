fca60b4ef3e58383897df2d0c4759f9c626c99af
Robert D. Hernandez <rherna70@uic.edu>
2025-03-28 20:01:18 +0000

    feature(patch-eval): remove some unused config items

--- src/patch-evaluation-service/patch_eval_config.py
+++ src/patch-evaluation-service/patch_eval_config.py
@@ -6,14 +6,12 @@ class PatchEvalConfig:
     version: str
     appname: str
     logging_config: str
-    crashes_events: str
     patch_eval_results_full_path: str
     patched_codes_path: str
     executables_full_path: str
     compiler_tool_full_path: str
     compiler_warning_flags: str
     compiler_feature_flags: str
-    temp_crashes_full_path: str
     compile_timeout: int
     run_timeout: int
     autopatch_crash_detail_input_topic: str
