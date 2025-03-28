68a041961d3ca50f9f97a4f6503f656cbb0a1c0f
Robert D. Hernandez <Rhernandez513@users.noreply.github.com>
2025-03-28 17:10:04 -0500

    Update test_fuzzing_service.py
    linter suggestions

--- src/fuzzing-service/test_fuzzing_service.py
+++ src/fuzzing-service/test_fuzzing_service.py
@@ -2,7 +2,9 @@ import base64
 import logging
 import os
 import subprocess
-from datetime import datetime as real_datetime, timezone, date
+from datetime import date
+from datetime import datetime as real_datetime
+from datetime import timezone
 from types import SimpleNamespace
 from unittest import mock
 
