6421e94ad253907253e1868244f58bcd9671dab9
Robert D. Hernandez <Rhernandez513@users.noreply.github.com>
2025-03-28 17:07:16 -0500

    Update __init__.py
    linter suggestions

--- src/autopatchshared/autopatchshared/__init__.py
+++ src/autopatchshared/autopatchshared/__init__.py
@@ -1,3 +1,3 @@
+from .get_current_timestamp import get_current_timestamp  # noqa: F401
 from .init_logging import init_logging  # noqa: F401
 from .load_config_from_json import load_config_as_json  # noqa: F401
-from .get_current_timestamp import get_current_timestamp  # noqa: F401
