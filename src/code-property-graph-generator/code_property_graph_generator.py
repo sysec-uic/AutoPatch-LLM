b0edf968435398c6a651503b6f570196475dbd26
Nico Gibson <ngibs01@jaguar.tamu.edu>
2025-03-30 02:15:05 +0000

    feature (cpg generator): added joern to devcontainer.json & updated config

--- src/code-property-graph-generator/code_property_graph_generator.py
+++ src/code-property-graph-generator/code_property_graph_generator.py
@@ -1,5 +1,6 @@
 import asyncio
 import base64
+from dataclasses import dataclass, field
 import glob
 import logging
 import logging.config
@@ -11,11 +12,34 @@ import sys
 from datetime import datetime
 from typing import Final, List
 
-from autopatchdatatypes import CpgDetail
+# from autopatchdatatypes import CpgDetail
 from autopatchpubsub import MessageBrokerClient
 from autopatchshared import init_logging, load_config_as_json, get_current_timestamp
 from cloudevents.http import CloudEvent
-from cpg_svc_config import CpgSvcConfig
+
+
+@dataclass
+class CpgDetail:
+    # vuln (vulnerable) - boolean flag indicating whether a CVE has been detected in this cpg
+    vuln: bool
+    # vuln_type - classification of detected vulnerability
+    vuln_type: str
+    graph: dict = field(default_factory=dict)
+
+
+@dataclass
+class CpgSvcConfig:
+    version: str
+    appname: str
+    logging_config: str
+    message_broker_host: str
+    message_broker_port: int
+    message_broker_protocol: str
+    concurrency_threshold: int
+    cpg_svc_output_topic: str
+    cpg_svc_input_codebase_path: str
+    cpg_svc_output_path: str
+
 
 # this is the name of the environment variable that will be used point to the configuration map file to load
 CONST_CPG_SVC_CONFIG: Final[str] = "CPG_SVC_CONFIG"
@@ -61,8 +85,10 @@ async def map_cpg_detail_as_cloudevent(cpg_detail: CpgDetail) -> CloudEvent:
     raise NotImplementedError
 
 
-def extract_code_property_graph(crash_details: List[CpgDetail], code_path: str) -> list[str]:
-    parse_command = ['joern-parse']
+def extract_code_property_graph(
+    crash_details: List[CpgDetail], code_path: str
+) -> list[str]:
+    parse_command = ["joern-parse"]
     cpg_list = []
 
     for filename in os.listdir(code_path):
@@ -75,48 +101,53 @@ def extract_code_property_graph(crash_details: List[CpgDetail], code_path: str) 
         if os.path.isfile(filepath):
             try:
                 process = subprocess.run(
-                    cmd,
-                    stdout = subprocess.PIPE,
-                    stderr = subprocess.PIPE,
-                    text=True
+                    cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True
                 )
 
                 cpg_list.append(output_filename)
             except Exception as e:
-                print(f'error processing {filename}: {e}')
+                print(f"error processing {filename}: {e}")
 
-    return cpg_list    
+    return cpg_list
 
 
-def write_code_property_graphs_as_csv(crash_details: List[CpgDetail], cpg_path: str) -> list[str]:
-    csv_gen_command = ['joern-export']
+def write_code_property_graphs_as_csv(
+    crash_details: List[CpgDetail], cpg_path: str
+) -> list[str]:
+    csv_gen_command = ["joern-export"]
     cpg_list = []
     for filename in os.listdir(cpg_path):
         filepath = os.path.join(cpg_path, filename)
         basename, ext = os.path.splitext(filename)
 
-        #TODO change path to service data directory
+        # TODO change path to service data directory
         output_dir = cpg_path + basename + os.sep
-        cmd = csv_gen_command + [filepath, "--out", output_dir, "--repr", "all", "--format", "neo4jcsv"]
+        cmd = csv_gen_command + [
+            filepath,
+            "--out",
+            output_dir,
+            "--repr",
+            "all",
+            "--format",
+            "neo4jcsv",
+        ]
 
         if os.path.isfile(filepath):
             try:
                 process = subprocess.run(
-                    cmd,
-                    stdout = subprocess.PIPE,
-                    stderr = subprocess.PIPE,
-                    text=True
+                    cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True
                 )
                 cpg_list.append(output_dir)
-                
+
             except Exception as e:
-                print(f'error processing {filename}: {e}')
+                print(f"error processing {filename}: {e}")
 
     return cpg_list
 
+
 def consolidate_csv(crash_details: List[CpgDetail], pattern: str) -> list[pd.DataFrame]:
-    data_suffix="_data.csv"
-    header_suffix="_header.csv"
+    data_suffix = "_data.csv"
+    header_suffix = "_header.csv"
     files = glob.glob(pattern)
     dfs = []
     for file in files:
@@ -124,13 +155,13 @@ def consolidate_csv(crash_details: List[CpgDetail], pattern: str) -> list[pd.Dat
         header_file = file.replace(data_suffix, header_suffix)
         if os.path.exists(header_file):
             # use header.csv to name columns
-            with open(header_file, 'r') as hf:
+            with open(header_file, "r") as hf:
                 header_line = hf.readline().strip()
-                columns = header_line.split(',')
+                columns = header_line.split(",")
             df = pd.read_csv(file, header=None, names=columns)
             dfs.append(df)
         else:
-            #TODO handle this differently
+            # TODO handle this differently
             print(f"Header file {header_file} not found for data file {file}.")
     if dfs:
         return pd.concat(dfs, ignore_index=True)
@@ -138,28 +169,32 @@ def consolidate_csv(crash_details: List[CpgDetail], pattern: str) -> list[pd.Dat
         # Handle this differently? Only get here if no dfs created
         return pd.DataFrame()
 
+
 def process_cpg_folders(crash_details: List[CpgDetail], csv_path: str) -> dict:
     cpg_df = {}
     for folder in os.listdir(csv_path):
         # ignore joern folder 'workspace'
-        if folder == "workspace": continue
+        if folder == "workspace":
+            continue
         folder_path = os.path.join(csv_path, folder)
         if os.path.isdir(folder_path):
             # Build glob patterns for node and edge data files
             nodes_pattern = os.path.join(folder_path, "nodes_*_data.csv")
             edges_pattern = os.path.join(folder_path, "edges_*_data.csv")
-            
+
             nodes_df = consolidate_csv(nodes_pattern)
             edges_df = consolidate_csv(edges_pattern)
-            
+
             cpg_df[folder] = {"nodes": nodes_df, "edges": edges_df}
     return cpg_df
 
+
+# TODO: change return to data object
 def scan_cpg(code_path: str) -> dict:
     results = {}
-    scan_command = ['joern-scan']
+    scan_command = ["joern-scan"]
     # force joern-scan to generate a new cpg
-    scan_command.append('--overwrite')
+    scan_command.append("--overwrite")
 
     for filename in os.listdir(code_path):
         filepath = os.path.join(code_path, filename)
@@ -171,30 +206,28 @@ def scan_cpg(code_path: str) -> dict:
                     scan_command + [filepath],
                     stdout=subprocess.PIPE,
                     stderr=subprocess.PIPE,
-                    text=True
+                    text=True,
                 )
 
                 output = process.stdout + process.stderr
                 result_line = None
                 for line in output.splitlines():
                     if line.startswith("Result:"):
-                        if result_line != None: # lookout for multiple 'result_line's
+                        if result_line != None:  # lookout for multiple 'result_line's
                             print("more than one result found!")
-                            print(f'result_line = {result_line}')
-                            print(f'line = {line}')
+                            print(f"result_line = {result_line}")
+                            print(f"line = {line}")
                         else:
                             result_line = line
 
                 results[filename] = result_line
 
             except Exception as e:
-                print(f'error processing {filename}: {e}')
+                print(f"error processing {filename}: {e}")
                 # Change behavior to list where result = error?
-                results[filename] = {
-                    'result': None,
-                    'error': str(e)
-                }
+                results[filename] = {"result": None, "error": str(e)}
     return results
+
 
 async def map_crashdetails_as_cloudevents(
     crash_details: List[CpgDetail],
@@ -216,16 +249,14 @@ async def produce_output(cpg_details: List[CpgDetail]) -> None:
 
     async def produce_event(event: CloudEvent) -> None:
         # flake8 flags the following as F821 because it doesn't recognize the global variable
-        logger.debug(
-            f"Producing on Topic: {config.cpg_svc_output_topic}"  # noqa: F821
-        )
+        logger.debug(f"Producing on Topic: {config.cpg_svc_output_topic}")  # noqa: F821
         logger.debug(f"Producing CloudEvent: {event}")
         message_broker_client.publish(
             config.cpg_svc_output_topic, str(event)  # noqa: F821
         )
 
-    cpg_details_cloud_events: List[CloudEvent] = (
-        await map_crashdetails_as_cloudevents(cpg_details)
+    cpg_details_cloud_events: List[CloudEvent] = await map_crashdetails_as_cloudevents(
+        cpg_details
     )
     message_broker_client: Final[MessageBrokerClient] = MessageBrokerClient(
         config.message_broker_host, config.message_broker_port, logger
@@ -241,7 +272,9 @@ async def produce_output(cpg_details: List[CpgDetail]) -> None:
         for event in cpg_details_cloud_events:
             await produce_event(event)
 
-    csv_path: Final[str] = os.path.join(config.cpg_svc_output_path, "code-property-graphs.csv")
+    csv_path: Final[str] = os.path.join(
+        config.cpg_svc_output_path, "code-property-graphs.csv"
+    )
     write_code_property_graphs_as_csv(cpg_details, csv_path)
 
 
@@ -265,8 +298,7 @@ async def main():
 
     CPG_SVC_START_TIMESTAMP: Final[str] = get_current_timestamp()
 
-
-    # TODO delete the fuzzing config implementation below and replace with CPG specific context 
+    # TODO delete the fuzzing config implementation below and replace with CPG specific context
 
     # _fuzz_svc_input_codebase_path: Final[str] = config.fuzz_svc_input_codebase_path
     # _fuzzer_tool_timeout_seconds: Final[int] = config.fuzzer_tool_timeout_seconds
@@ -281,17 +313,30 @@ async def main():
     # _afl_compiler_tool_full_path: Final[str] = config.afl_compiler_tool_full_path
 
     logger.info("AppVersion: " + config.version)
-    # TODO Update the config names and values 
+    # TODO Update the config names and values
     # logger.info("CPG tool name: " + config.fuzzer_tool_name)
     # logger.info("CPG tool version: " + config.fuzzer_tool_version)
 
-    # TODO 
+    # TODO
     # do the thing
+    scan_results = scan_cpg(config.cpg_svc_input_codebase_path)
+    print(scan_results)
+
+    cpg_list = extract_code_property_graph(config.cpg_svc_input_codebase_path)
+    print(cpg_list)
+    # TODO adjust to handle timestamped directory
+    # write_code_property_graphs_as_csv() looks for .cpg files in passed directory
+    # process_cpg_folders() looks for subdirectories in passed directory
+    # therefore it should be ok to use same timestamped directory
+    csv_list = write_code_property_graphs_as_csv(config.cpg_svc_output_path)
+    print(csv_list)
+    cpg_df = process_cpg_folders(config.cpg_svc_output_path)
+    print(cpg_df)
 
     CPG_SVC_END_TIMESTAMP: Final[str] = get_current_timestamp()
-    time_delta = datetime.fromisoformat(
-        CPG_SVC_END_TIMESTAMP
-    ) - datetime.fromisoformat(CPG_SVC_START_TIMESTAMP)
+    time_delta = datetime.fromisoformat(CPG_SVC_END_TIMESTAMP) - datetime.fromisoformat(
+        CPG_SVC_START_TIMESTAMP
+    )
     logger.info(f"Total Processing Time Elapsed: {time_delta}")
     logger.info("Processing complete, exiting.")
 
