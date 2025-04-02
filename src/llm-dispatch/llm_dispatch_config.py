340949de2bb8536c1c56e92a18682d0b6a18aacb
Robert D. Hernandez <rherna70@uic.edu>
2025-04-02 01:18:26 +0000

    feature(llm-dispatch): more fluent invocation of llms using prompt composition and injection of context and configuration, runs to completion now

--- src/llm-dispatch/llm_dispatch_config.py
+++ src/llm-dispatch/llm_dispatch_config.py
@@ -1,23 +1,32 @@
 from dataclasses import dataclass
-from typing import Dict
+from typing import Dict, List
 
 
 @dataclass
 class LLMDispatchConfig:
-    version: str
-    appname: str
+    appName: str
+    appVersion: str
+    appDescription: str
     logging_config: str
-    concurrency_threshold: int
     message_broker_host: str
     message_broker_port: int
-    message_broker_protocol: str
-    temperature: float
-    top_p: float
-    max_tokens: int
-    api_models: Dict[str, str]
-    api_providers: Dict[str, str]
-    api_secrets: Dict[str, str]
-    in_memory_models: Dict[str, str]
-    in_memory_providers: Dict[str, str]
-    # in_memory_secrets: Dict[str, str]
-    model_router: Dict
+    message_broker_patch_request_topic: str
+    message_broker_patch_response_topic: str
+    message_broker_client_id: str
+    systemprompt_full_path: str
+    userprompt_full_path: str
+    model_router_base_url: str
+    model_router_fallback_model: str
+    model_router_max_concurrent_requests: int
+    api_timeout_seconds: int
+    api_retry_attempts: int
+    api_retry_delay_ms: int
+    default_max_tokens: int
+    default_temperature: int
+    default_top_p: int
+    models: List[Dict[str, str]]
+    # "default_frequency_penalty": 0,
+    # "default_presence_penalty": 0,
+    # "cache_enabled": false,
+    # "cache_dir": "/workspace/AutoPatch-LLM/src/llm-dispatch/data/cache",
+    # "cache_expiration": 3600,
