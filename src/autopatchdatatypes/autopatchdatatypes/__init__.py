9d0c526ef3e127af6e9980d64a627f4bf99fc810
Robert D. Hernandez <rherna70@uic.edu>
2025-04-03 02:07:06 +0000

    refactor(autopatchdatatypes): comment unused code

--- src/autopatchdatatypes/autopatchdatatypes/__init__.py
+++ src/autopatchdatatypes/autopatchdatatypes/__init__.py
@@ -1,3 +1,3 @@
 from .crash_detail import CrashDetail  # noqa: F401
-from .patch_request import PatchRequest  # noqa: F401
-from .patch_response import PatchResponse  # noqa: F401
+# from .patch_request import PatchRequest  # noqa: F401
+# from .patch_response import PatchResponse  # noqa: F401
